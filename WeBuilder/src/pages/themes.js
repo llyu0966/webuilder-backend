c3e794b04cb9ab66dcdb7cebac4b009a720a4183
Anastasia Gusenkov <anastasiagusenkov1@gmail.com>
2021-12-09 15:25:31 -0500

    update theme

--- WeBuilder/src/pages/themes.js
+++ WeBuilder/src/pages/themes.js
@@ -15,28 +15,30 @@ function Themes() {
                 <style>{'body { background: #6D44C5; }'}</style>
             </Helmet>
             <div class="offset container mt-3 mb-5">
+            <div class="row">
+                        <h3 id="headerH3">Themes</h3>
+                        <h5 id="headerH5">Choose the theme of your website.</h5>
+                    </div>
                 <div class="row">
-                    <div id="navBar" class="col-lg-4 col-md-4 col-12">
+                    <div id="navBar" class="col-lg-2 col-md-4 col-12">
                         <NavBar />
 
                     </div>
 
                     <div id="content" class="col-lg-8 col-md-8 col-12">
-                        <h3 id="headerH3">Themes</h3>
-                        <h5 id="headerH5">Choose the theme of your website.</h5>
+                        
                         <ThemeLayouts />
+                    </div>
+
+                    <div id="nextButton" class="col-lg-2 col-md-2 col-12">
+                            <button onClick={() => {
+                             history.push('/aboutMe')
+                            }} href={'/aboutMe'} id="next"><ArrowRightSquare/></button>
                     </div>
                 </div>
 
             </div>
-            <button onClick={() => {
-                history.push('/categories')
-            }} href={'/categories'} id="back">Back</button>
-
-            <button onClick={() => {
-                history.push('/aboutMe')
-            }} href={'/aboutMe'} id="next">Next</button>
-
+            
         </div>
     );
 }
